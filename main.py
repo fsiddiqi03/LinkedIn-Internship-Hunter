da97ab38177f06e982a9dc4c2c92c4e1eba03b13
fsiddiqi03 <farissiddiqi@gmail.com>
2024-04-13 22:47:43 -0500

    connected python script to google sheet

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ import os
 load_dotenv()
 
 
-
 scopes = {
     os.getenv('SCOPE')
 }
+
+creds = Credentials.from_service_account_file("creds.json", scopes=scopes)
+client = gspread.authorize(creds)
+
+
+sheet = client.open_by_key(os.getenv('SHEET_ID'))
+
+values = sheet.sheet1.row_values(1)
+print(values)
