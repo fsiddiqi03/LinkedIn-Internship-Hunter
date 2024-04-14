413c3afc25cd6470f53b0549a65486a3fbaa3e74
fsiddiqi03 <farissiddiqi@gmail.com>
2024-04-13 23:42:21 -0500

    added test values

--- main.py
+++ main.py
@@ -6,15 +6,32 @@ import os
 load_dotenv()
 
 
-scopes = {
-    os.getenv('SCOPE')
-}
+scopes = [os.getenv('SCOPE')]
 
 creds = Credentials.from_service_account_file("creds.json", scopes=scopes)
 client = gspread.authorize(creds)
 
 
-sheet = client.open_by_key(os.getenv('SHEET_ID'))
+# Open the Google Sheet by ID and get the spread sheet
+sheet = client.open_by_key(os.getenv('SHEET_ID')).sheet1  
 
-values = sheet.sheet1.row_values(1)
-print(values)
+jobs = [
+    {
+        "company": "Microsoft",
+        "job_title": "Product Management: Intern Opportunities for University Students",
+        "location": "New York, NY",
+        "job_url": "https://www.linkedin.com/jobs/view/product-management-intern-opportunities-for-university-students-at-microsoft-3203330682"
+    },
+    {
+        "company": "Microsoft",
+        "job_title": "Content Strategist",
+        "location": "United States",
+        "job_url": "https://www.linkedin.com/jobs/view/content-strategist-at-microsoft-3257692764"
+    }
+]
+
+# Prepare data for insertion
+values = [[job['company'], job['job_title'], job['location'], job['job_url'], 'Need To Apply'] for job in jobs]
+
+# Append data into the sheet
+sheet.append_rows(values)  
